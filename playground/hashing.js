ae343b7fd7ca203c0b053a677a0c2a989f118837
IdamEniIdam <idameni89@gmail.com>
2018-09-30 13:57:24 +1400

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,17 +1,31 @@
 const {SHA256} = require('crypto-js');
-
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+var password = '123abc!'
+
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
+
+var hashedPassword = '$2a$10$QBFlzUU/svKeQ04nizlTg.nQRkfp6gZ79wE6Ov57lBc8WYnzilBpi';
+
+bcrypt.compare('password', hashedPassword, (err, res) => {
+    console.log(res);
+})
 
 
-var data = {
-    id: 10
-};
+// var data = {
+//     id: 10
+// };
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 
 
