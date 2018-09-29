4aaeab440f4c612d96c9cbfea86f6ac6da9b90dc
IdamEniIdam <idameni89@gmail.com>
2018-09-30 12:51:33 +1400

    Add GET /users/me and authentication middle

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 //to connect to herokus
@@ -120,6 +121,21 @@ user.save().then(() => {
 }).catch((e) => {
     res.status(400).send(e);
     })
+});
+
+
+app.get('/users/me', authenticate, (req, res) => {
+    var token = req.header('x-auth');
+
+    User.findByToken(token).then((user) => {
+        if (!user) {
+           return Promise.reject(); 
+        }
+
+        res.send(user);
+    }).catch((e) => {
+        res.status(401).send();
+    });
 });
 
 app.listen(port, () => {
